705b49babbb428b214a12f54d0d47a58413bec48
github username <github email address>
2018-06-29 08:46:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+const token = 'YOUR_TOKEN_HERE' //Wouldn't putting my github token in here be a bad security idea?
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
